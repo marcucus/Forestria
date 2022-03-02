26d6d73aab9ebb2e17d439f81a49e635cd44eb46
DimitriDenis <47151006+DimitriDenis@users.noreply.github.com>
2022-03-02 18:19:20 +0100

    tracé oiseaux

--- public/js/map.js
+++ public/js/map.js
@@ -26,5 +26,24 @@ marker.bindPopup(contenu);
 
 
 
+// create a red polyline from an array of LatLng points
+const latlngs = [
+    [45.838660, 1.234474],
+    [45.837954, 1.232736],
+    [45.836186, 1.232371],
+    [45.835198, 1.234785],
+    [45.834661, 1.240384],
+    [45.834376, 1.243417],
+    [45.835531, 1.241751],
+    [45.835793, 1.238964],
+    [45.836212, 1.236158],
+    [45.838080, 1.235173],
+    [45.838116, 1.236647],
+    [45.838136, 1.238206],
+];
 
+var polyline = L.polyline(latlngs, {color: 'red'}).addTo(mymap);
+
+// zoom the map to the polyline
+mymap.fitBounds(polyline.getBounds());
 
