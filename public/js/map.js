d5f3c7321449bf562f7c0203e07b1b448cce3032
Adrien Marques <adrien.marques@etu.unilim.fr>
2022-03-15 18:30:12 +0100

    Main:config map.js

--- public/js/map.js
+++ public/js/map.js
@@ -1,7 +1,6 @@
-
 const apiKey='pk.eyJ1IjoiY2hhaW1hYWJyIiwiYSI6ImNsMDVmbTFwcjB2Zm8zYm1qZHgyaTJuY3UifQ.YjE5dig22P0cEMnAh2cznA';
-let points = $('#points').data("isPoints");
 
+let points = $('#points').data("isPoints");
 
 let plati = $('#platitude').data("isPlatitude");
 let plongi = $('#plongitude').data("isPlongitude");
@@ -9,10 +8,14 @@ let plongi = $('#plongitude').data("isPlongitude");
 let lati = $('#latitude').data("isLatitude");
 let longi = $('#longitude').data("isLongitude");
 
+var nb= points.length;
+
 console.log(lati,longi);
+
 points.forEach(function(item,index,array) {
     console.log(item,index);
 });
+
 
 const mymap = L.map('map').setView([lati, longi],13);
 
@@ -25,33 +28,29 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
 }).addTo(mymap);
 
 
-const marker = L.marker([45.8372447257086, 1.2378704867778718]).addTo(mymap);
-let contenu = `
-<h5> Campus La Borie</h5>
-<div style="text-align:center">
-    <img width="95" height="95" src="../../public/images/sentier_handicap.png" alt="Image du point"/>
-    <button ion-button clear round (click)="InfosParcour()">En savoir plus</button>
-    </div>
-    `
 
-marker.bindPopup(contenu);
+const latlngs = [];
 
+for(let i =0; i < nb; i++)
+{
 
-const latlngs = [
-    [45.838660, 1.234474],
-    [45.837954, 1.232736],
-    [45.836186, 1.232371],
-    [45.835198, 1.234785],
-    [45.834661, 1.240384],
-    [45.834376, 1.243417],
-    [45.835531, 1.241751],
-    [45.835793, 1.238964],
-    [45.836212, 1.236158],
-    [45.838080, 1.235173],
-    [45.838116, 1.236647],
-    [45.838136, 1.238206],
-];
-
+    latlngs.push([points[i]['latitude'],points[i]['longitude']],);
+    marker = L.marker([points[i]['latitude'],points[i]['longitude']]).addTo(mymap);
+    contenu = `
+        <h5>`
+    contenu += points[i]['titre'];
+    contenu +=`</h5>
+        <div style="text-align:center">
+            <img width="95" height="95" src="../../public/images/sentier_handicap.png" alt="Image du point"/>
+            <p>`
+    contenu += points[i]['text'];
+    contenu+= `</p>
+            <button ion-button clear round (click)="InfosParcour()">En savoir plus</button>
+        </div>
+        `
+    marker.bindPopup(contenu);
+}
+console.log(latlngs);
 var polyline = L.polyline(latlngs, {color: 'red'}).addTo(mymap);
 
 
