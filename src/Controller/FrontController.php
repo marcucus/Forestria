adf9f48ddb56e5776037fcbdfafc8079c09b6000
Adrien Marques <adrien.marques@etu.unilim.fr>
2022-03-17 03:28:46 +0100

    Main:Add visu

--- src/Controller/FrontController.php
+++ src/Controller/FrontController.php
@@ -2,17 +2,27 @@
 
 namespace App\Controller;
 
+use App\Repository\ParcoursRepository;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class FrontController extends AbstractController
 {
-    #[Route('/', name: 'front')]
-    public function index(): Response
+    #[Route('/', name: 'front', methods: ['GET'])]
+    public function index(ParcoursRepository $parcoursRepository, PaginatorInterface $paginator, Request $request): Response
     {
+        $parcQuery = $parcoursRepository->findAll();
+        $parcours = $paginator->paginate(
+            $parcQuery, /* query NOT result */
+            $request->query->getInt('page', 1), /*page number*/
+            10 /*limit per page*/
+        );
         return $this->render('front/index.html.twig', [
-            'controller_name' => 'FrontController',
+            'parcours' => $parcours,
         ]);
+
     }
 }
