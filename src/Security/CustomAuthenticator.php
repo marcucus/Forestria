25ba24e72d8ce6bba0a0b254925f0a5f33bbf9c7
Adrien Marques <adrien.marques@etu.unilim.fr>
2022-03-21 19:30:46 +0100

    Main:Fix bugs

--- src/Security/CustomAuthenticator.php
+++ src/Security/CustomAuthenticator.php
@@ -50,7 +50,7 @@ class CustomAuthenticator extends AbstractLoginFormAuthenticator
         }
 
         // For example:
-        return new RedirectResponse($this->urlGenerator->generate('front'));
+        return new RedirectResponse($this->urlGenerator->generate('app_front'));
         throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
     }
 
